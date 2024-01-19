ae4a329ea1295a8b790a21bc5e5a69b450202677
danil <onefiler1@hotmail.com>
2024-01-19 23:33:48 +0200

    add binding itself

---
+++ src/RayGui-cs/RayGui.Wrappers.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+using Raylib_cs;
+
+namespace RayGui_cs;
+
+public static partial class RayGui
+{
+    public static unsafe ReadOnlySpan<uint> GuiGetIconsAsSpan()
+    {
+        return new ReadOnlySpan<uint>(GuiGetIcons(), IconsArraySize);
+    }
+    
+    public static unsafe int GuiTabBar(Rectangle bounds, string[] text, ref int active)
+    {
+        using var nativeArray = text.GetNativeArray();
+        return GuiTabBar(bounds, nativeArray, text.Length, ref active);
+    }
+
+    public static unsafe int GuiListViewEx(Rectangle bounds, string[] text, ref int scrollIndex, ref int active,
+        ref int focus)
+    {
+        using var nativeArray = text.GetNativeArray();
+        return GuiListViewEx(bounds, nativeArray, text.Length, ref scrollIndex, ref active, ref focus);
+    }
+}
---
+++ src/RayGui-cs/RayGui.cs
@@ -0,0 +1,206 @@
+﻿using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+using Raylib_cs;
+
+namespace RayGui_cs;
+
+public static partial class RayGui
+{
+    public const int VersionMajor = 4;
+    public const int VersionMinor = 0;
+    public const int VersionPatch = 0;
+
+    public static readonly Version Version = new Version(VersionMajor, VersionMajor, VersionPatch);
+
+    public const string LibraryName = "raygui";
+
+    public const int IconSize = 16;
+    public const int MaxIcons = 256;
+    public const int MaxNameLength = 32;
+    public const int IconDataElements = IconSize * IconSize / 32;
+
+    public const int IconsArraySize = MaxIcons * IconDataElements;
+
+    public const int MaxControls = 16;
+    public const int MaxPropsBase = 16;
+    public const int MaxPropsExtended = 8;
+    
+    [LibraryImport(LibraryName)]
+    public static partial void GuiEnable();
+    
+    [LibraryImport(LibraryName)]
+    public static partial void GuiDisable();
+
+    [LibraryImport(LibraryName)]
+    public static partial void GuiLock();
+
+    [LibraryImport(LibraryName)]
+    public static partial void GuiUnlock();
+
+    [LibraryImport(LibraryName)]
+    public static partial bool GuiIsLocked();
+
+    [LibraryImport(LibraryName)]
+    public static partial void GuiSetAlpha(float alpha);
+
+    [LibraryImport(LibraryName)]
+    public static partial void GuiSetState(GuiState state);
+
+    [LibraryImport(LibraryName)]
+    public static partial GuiState GuiGetState();
+
+    [LibraryImport(LibraryName)]
+    public static partial void GuiSetFont(Font font);
+
+    [LibraryImport(LibraryName)]
+    public static partial Font GuiGetFont();
+
+    [LibraryImport(LibraryName)]
+    public static partial void GuiSetStyle(int control, int property, int value);
+
+    [LibraryImport(LibraryName)]
+    public static partial int GuiGetStyle(int control, int property);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static partial void GuiLoadStyle(string fileName);
+
+    [LibraryImport(LibraryName)]
+    public static partial void GuiLoadStyleDefault();
+
+    [LibraryImport(LibraryName)]
+    public static partial void GuiEnableTooltip();
+
+    [LibraryImport(LibraryName)]
+    public static partial void GuiDisableTooltip();
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static partial void GuiSetTooltip(string tooltip);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [return: MarshalAs(UnmanagedType.LPUTF8Str)]
+    public static partial string GuiIconText(int iconId, string text);
+
+    [LibraryImport(LibraryName)]
+    public static partial void GuiSetIconScale(int scale);
+
+    [LibraryImport(LibraryName)]
+    public static unsafe partial uint* GuiGetIcons();
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial byte** GuiLoadIcons(string fileName, bool loadIconsName);
+
+    [LibraryImport(LibraryName)]
+    public static partial void GuiDrawIcon(int iconId, int posX, int posY, int pixelSize, Color color);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static partial int GuiWindowBox(Rectangle bounds, string title);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static partial int GuiGroupBox(Rectangle bounds, string text);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static partial int GuiLine(Rectangle bounds, string text);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static partial int GuiPanel(Rectangle bounds, string text);
+
+    [LibraryImport(LibraryName)]
+    public static unsafe partial int GuiTabBar(Rectangle bounds, sbyte** text, int count, ref int active);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiScrollPanel(Rectangle bounds, string text, Rectangle content,
+        ref Vector2 scroll, ref Rectangle view);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiLabel(Rectangle bounds, string text);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiButton(Rectangle bounds, string text);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiLabelButton(Rectangle bounds, string text);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiToggle(Rectangle bounds, string text, ref bool active);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiToggleGroup(Rectangle bounds, string text, ref int active);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiToggleSlider(Rectangle bounds, string text, ref int active);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiCheckBox(Rectangle bounds, string text, ref bool @checked);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiComboBox(Rectangle bounds, string text, ref int active);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiDropdownBox(Rectangle bounds, string text, ref int active, bool editMode);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiSpinner(Rectangle bounds, string text, ref int value, int minValue,
+        int maxValue, bool editMode);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiValueBox(Rectangle bounds, string text, ref int value, int minValue,
+        int maxValue, bool editMode);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiTextBox(Rectangle bounds, ref string text, int textSize, bool editMode);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiSlider(Rectangle bounds, string textLeft, string textRight, ref float value,
+        float minValue, float maxValue);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiSliderBar(Rectangle bounds, string textLeft, string textRight, ref float value,
+        float minValue, float maxValue);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiProgressBar(Rectangle bounds, string textLeft, string textRight,
+        ref float value, float minValue, float maxValue);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiStatusBar(Rectangle bounds, string text);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiDummyRec(Rectangle bounds, string text);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiGrid(Rectangle bounds, string text, float spacing, int subdivs,
+        ref Vector2 mouseCell);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiListView(Rectangle bounds, string text, out int scrollIndex, ref int active);
+
+    [LibraryImport(LibraryName)]
+    public static unsafe partial int GuiListViewEx(Rectangle bounds, sbyte** text, int count, ref int scrollIndex,
+        ref int active, ref int focus);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiMessageBox(Rectangle bounds, string title, string message, string buttons);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiTextInputBox(Rectangle bounds, string title, string message, string buttons,
+        ref string text, int textMaxSize, ref bool secretViewActive);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiColorPicker(Rectangle bounds, string text, ref Color color);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiColorPanel(Rectangle bounds, string text, ref Color color);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiColorBarAlpha(Rectangle bounds, string title, ref float alpha);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiColorBarHue(Rectangle bounds, string text, ref float value);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiColorPickerHSV(Rectangle bounds, string text, ref Vector3 colorHsv);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial int GuiColorPanelHSV(Rectangle bounds, string text, ref Vector3 colorHsv);
+}

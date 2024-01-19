42982bb177192fefa798fc7e38eb67ce4dc75232
danil <onefiler1@hotmail.com>
2024-01-19 23:02:24 +0200

    add utility class to work with sbyte**

---
+++ src/RayGui-cs/Utf8StringArray.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace RayGui_cs;
+
+public readonly struct Utf8StringArray : IDisposable
+{
+    public readonly unsafe sbyte** Handle;
+    public readonly nuint Count;
+    
+    public unsafe Utf8StringArray(string[] array)
+    {
+        Count = (nuint)array.Length;
+        Handle = (sbyte**)NativeMemory.Alloc(Count, (uint)sizeof(nint));
+
+        for (nuint i = 0; i < Count; i++)
+        {
+            Handle[i] = (sbyte*)Marshal.StringToCoTaskMemUTF8(array[i]);
+        }
+    }
+
+    public unsafe Utf8StringArray(sbyte** handle, nuint count)
+    {
+        Handle = handle;
+        Count = count;
+    }
+
+    public unsafe string[] ToArray()
+    {
+        var arr = new string[Count];
+        
+        for (nuint i = 0; i < Count; i++)
+        {
+            arr[i] = Marshal.PtrToStringUTF8((IntPtr)Handle[i]) ?? string.Empty;
+        }
+
+        return arr;
+    }
+
+    public unsafe void Dispose()
+    {
+        for (nuint i = 0; i < Count; i++)
+        {
+            Marshal.FreeCoTaskMem((IntPtr)Handle[i]);
+        }
+        
+        NativeMemory.Free(Handle);
+    }
+
+    public static unsafe implicit operator sbyte**(Utf8StringArray array) => array.Handle;
+}
---
+++ src/RayGui-cs/Utf8StringArrayUtils.cs
@@ -0,0 +1,18 @@
+﻿using Raylib_cs;
+
+namespace RayGui_cs;
+
+public static class Utf8StringArrayUtils
+{
+    public static Utf8StringArray GetNativeArray(this string[] array)
+    {
+        return new Utf8StringArray(array);
+    }
+
+    public static unsafe string[] GetArray(sbyte** handle, int count)
+    {
+        using var nativeArray = new Utf8StringArray(handle, (nuint)count);
+        var array = nativeArray.ToArray();
+        return array;
+    }
+}
